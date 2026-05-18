666a47885408e5fda634c5a4fed5f69e3bd093aa
pedrojuliolantigua1-art <pedrojuliolantigua1@gmail.com>
2026-05-18 19:47:56 -0400

    Agregar archivos de proyecto.

---
+++ Sistema de nomina/Empleado.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Sistema_de_nomina
+{
+    public abstract class Empleado
+    {
+        private String apellidoPaterno { get; set; }
+        private String numeroSeguroSocial { get; set; }
+
+        public abstract decimal Ingresos();
+
+    }
+}
